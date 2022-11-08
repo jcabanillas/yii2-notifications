8abe274e58d7f25276ecc8f95a8d5f90cd073fa1
Luis Javier Cabanillas Champac <jcabanillas@rubiko.mx>
2022-11-08 11:30:46 -0600

    Cambios en WIDGET

--- widgets/Notifications.php
+++ widgets/Notifications.php
@@ -25,11 +25,22 @@ class Notifications extends \yii\base\Widget
         'class' => 'dropdown-toggle',
         'data-toggle' => 'dropdown'
     ];
+    public $spanContent = <<<HTML
+												<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Code/Compiling.svg-->
+												<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<path d="M11.2929 2.70711C11.6834 2.31658 12.3166 2.31658 12.7071 2.70711L15.2929 5.29289C15.6834 5.68342 15.6834 6.31658 15.2929 6.70711L12.7071 9.29289C12.3166 9.68342 11.6834 9.68342 11.2929 9.29289L8.70711 6.70711C8.31658 6.31658 8.31658 5.68342 8.70711 5.29289L11.2929 2.70711Z" fill="currentColor"></path>
+												<path d="M11.2929 14.7071C11.6834 14.3166 12.3166 14.3166 12.7071 14.7071L15.2929 17.2929C15.6834 17.6834 15.6834 18.3166 15.2929 18.7071L12.7071 21.2929C12.3166 21.6834 11.6834 21.6834 11.2929 21.2929L8.70711 18.7071C8.31658 18.3166 8.31658 17.6834 8.70711 17.2929L11.2929 14.7071Z" fill="currentColor"></path>
+												<path opacity="0.3" d="M5.29289 8.70711C5.68342 8.31658 6.31658 8.31658 6.70711 8.70711L9.29289 11.2929C9.68342 11.6834 9.68342 12.3166 9.29289 12.7071L6.70711 15.2929C6.31658 15.6834 5.68342 15.6834 5.29289 15.2929L2.70711 12.7071C2.31658 12.3166 2.31658 11.6834 2.70711 11.2929L5.29289 8.70711Z" fill="currentColor"></path>
+												<path opacity="0.3" d="M17.2929 8.70711C17.6834 8.31658 18.3166 8.31658 18.7071 8.70711L21.2929 11.2929C21.6834 11.6834 21.6834 12.3166 21.2929 12.7071L18.7071 15.2929C18.3166 15.6834 17.6834 15.6834 17.2929 15.2929L14.7071 12.7071C14.3166 12.3166 14.3166 11.6834 14.7071 11.2929L17.2929 8.70711Z" fill="currentColor"></path>
+											</svg>
+												<!--end::Svg Icon-->
+											
+HTML;
     public $spanOptions = [
         'class' => 'glyphicon glyphicon-bell'
     ];
     public $headerTag = 'div';
-    
+
     public $headerOptions = [];
     public $menuOptions = [];
     public $footerOptions = [];
@@ -86,21 +97,26 @@ class Notifications extends \yii\base\Widget
     protected function renderNavbarItem()
     {
         $html = Html::beginTag($this->containerTag, $this->options);
-        $html .= Html::beginTag($this->linkTag, $this->linkOptions);
-        $html .= Html::tag('span', '', $this->spanOptions);
 
         $count = self::getCountUnseen();
+        /*
         $countOptions = array_merge([
             'tag' => 'span',
             'data-count' => $count,
         ], $this->countOptions);
         Html::addCssClass($countOptions, 'label label-warning navbar-badge notifications-count');
-        if (!$count) {
-            $countOptions['style'] = 'display: none;';
+        */
+        if ($count) {
+            // $countOptions['style'] = 'display: none;';
+            $html .= Html::beginTag($this->linkTag, array_merge($this->linkOptions, ['class' => 'pulse pulse-primary']));
+        } else {
+            $html .= Html::beginTag($this->linkTag, array_merge($this->linkOptions));
         }
-        $countTag = ArrayHelper::remove($countOptions, 'tag', 'span');
-        $html .= Html::tag($countTag, $count, $countOptions);
-
+        $html .= Html::beginTag($this->spanTag, $this->spanOptions);
+        // $countTag = ArrayHelper::remove($countOptions, 'tag', 'span');
+        // $html .= Html::tag($countTag, $count, $countOptions);
+        $html .= $this->spanContent;
+        $html .= Html::endTag($this->spanTag);
         $html .= Html::endTag($this->linkTag);
         $html .= Html::begintag('div', $this->menuOptions);
         $header = Html::a(Yii::t('modules/notifications', 'Mark all as read'), '#', ['class' => 'read-all pull-right']);
