c27fae219dbbd18c43e214d653d9dc07d0bc0125
Luis Javier Cabanillas Champac <jcabanillas@rubiko.mx>
2022-11-08 12:08:35 -0600

    Otro Fix de Notifications Widget

--- widgets/Notifications.php
+++ widgets/Notifications.php
@@ -138,7 +138,12 @@ HTML;
         //$html .= Html::tag('div', '<span class="ajax-loader"></span>', ['class' => 'loading-row']);
         $html .= Html::tag('div', Html::tag('span', Yii::t('modules/notifications', 'There are no notifications to show'), ['style' => 'display: none;']), ['class' => 'empty-row']);
         $html .= Html::endTag('div');
-        $this->footer = !is_null($this->footer) ?: Html::a(Yii::t('modules/notifications', 'View all'), ['/notifications/default/index']);
+        $this->footer = Html::a(Yii::t('modules/notifications', 'View all') . ' <span class="svg-icon svg-icon-5">
+														<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+															<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor"></rect>
+															<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor"></path>
+														</svg>
+													</span>', ['/notifications/default/index'], ['class' => "btn btn-color-gray-600 btn-active-color-primary"]); // Html::a(Yii::t('modules/notifications', 'View all'), ['/notifications/default/index']);
         $html .= Html::tag('div', $this->footer, $this->footerOptions);
         $html .= Html::endTag('div');
         $html .= Html::endTag($this->containerTag);
